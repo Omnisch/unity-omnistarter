d04111d3f4995908eacf7628d0b70a91569c3aa3
Omnisch <fleurdeln@gmail.com>
2025-03-16 14:35:08 +0800

    add functions to YieldTweaker

--- Omnistarter/Scripts/Tweakers/YieldTweaker.cs
+++ Omnistarter/Scripts/Tweakers/YieldTweaker.cs
@@ -1,5 +1,5 @@
 // author: Omnistudio
-// version: 2025.03.15
+// version: 2025.03.16
 
 using System.Collections;
 using UnityEngine;
@@ -9,50 +9,18 @@ namespace Omnis.Util
 {
     public class YieldTweaker
     {
-        #region Linear Accumulation
+        #region Accumulation
         /// <summary>
         /// It takes <i>time</i> seconds to accumulate from 0 to 1.
         /// </summary>
-        public static IEnumerator Linear(UnityAction<float> action, float time = 1f)
+        public static IEnumerator Linear(UnityAction<float> action, float time = 1f, bool fixedUpdate = false)
         {
             var life = 0f;
             while (life < 1f)
             {
                 action?.Invoke(life);
-                life += Time.deltaTime / time;
-                yield return null;
-            }
-            action?.Invoke(1f);
-        }
-
-        /// <summary>
-        /// It takes <i>time</i> seconds to accumulate from 0 to 1.
-        /// </summary>
-        public static IEnumerator LinearFixed(UnityAction<float> action, float time = 1f)
-        {
-            var life = 0f;
-            while (life < 1f)
-            {
-                action?.Invoke(life);
-                life += Time.fixedDeltaTime / time;
-                yield return new WaitForFixedUpdate();
-            }
-            action?.Invoke(1f);
-        }
-        #endregion
-
-        #region Lerp Accumulation
-        /// <summary>
-        /// It continuously lerps from 0 to 1 with <i>speed</i>.
-        /// </summary>
-        public static IEnumerator Lerp(UnityAction<float> action, float speed = 1f)
-        {
-            var life = 0f;
-            while (1f - life > 0.01f)
-            {
-                action?.Invoke(life);
-                life = Mathf.Lerp(life, 1f, speed * Time.deltaTime);
-                yield return null;
+                life += (fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime) / time;
+                yield return fixedUpdate ? new WaitForFixedUpdate() : null;
             }
             action?.Invoke(1f);
         }
@@ -60,16 +28,53 @@ namespace Omnis.Util
         /// <summary>
         /// It continuously lerps from 0 to 1 with <i>speed</i>.
         /// </summary>
-        public static IEnumerator LerpFixed(UnityAction<float> action, float speed = 1f)
+        public static IEnumerator Lerp(UnityAction<float> action, float speed = 1f, bool fixedUpdate = false)
         {
             var life = 0f;
             while (1f - life > 0.01f)
             {
                 action?.Invoke(life);
-                life = Mathf.Lerp(life, 1f, speed * Time.fixedDeltaTime);
-                yield return new WaitForFixedUpdate();
+                life = Mathf.Lerp(life, 1f, speed * (fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime));
+                yield return fixedUpdate ? new WaitForFixedUpdate() : null;
             }
             action?.Invoke(1f);
+        }
+
+        /// <summary>
+        /// It imitates an elastic motion from 0 to 1.
+        /// </summary>
+        public static IEnumerator Elastic(UnityAction<float> action, float speed = 1f, float drag = 0.1f, bool fixedUpdate = false)
+        {
+            var amp = 1f;
+            var life = 0f;
+            var value = 0f;
+            while (amp > 0.01f)
+            {
+                action?.Invoke(value);
+                value = amp * Mathf.Sin(speed * life);
+                amp = Mathf.Exp(-drag * life);
+                Debug.Log(amp);
+                life += fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime;
+                yield return fixedUpdate ? new WaitForFixedUpdate() : null;
+            }
+            action?.Invoke(1f);
+        }
+
+        /// <summary>
+        /// It draws a sine wave from x=0.
+        /// </summary>
+        public static IEnumerator SineWave(UnityAction<float> action, float amplitude = 1f, float frequency = 1f, float cycleCount = 1f, bool fixedUpdate = false)
+        {
+            var life = 0f;
+            var value = 0f;
+            while (life < cycleCount / frequency)
+            {
+                action?.Invoke(value);
+                value = amplitude * Mathf.Sin(frequency * life * 2f * Mathf.PI);
+                life += fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime;
+                yield return fixedUpdate ? new WaitForFixedUpdate() : null;
+            }
+            action?.Invoke(amplitude * Mathf.Sin(cycleCount * 2f * Mathf.PI));
         }
         #endregion
 
