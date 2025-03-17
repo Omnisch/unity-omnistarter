142beff192b8732b03aecceaba53bef7e10ca363
Omnisch <fleurdeln@gmail.com>
2025-03-17 18:12:57 +0800

    refine Loop to be more compatible

--- Omnistarter/Scripts/Helpers/YieldHelper.cs
+++ Omnistarter/Scripts/Helpers/YieldHelper.cs
@@ -29,6 +29,11 @@ namespace Omnis.Utils
             action?.Invoke(1f);
         }
 
+        /// <summary>
+        /// It takes <i>time</i> seconds to ease from 0 to 1, repeat <i>cycleCount</i> times.
+        /// </summary>
+        /// <param name="pingPong">If true, it will use Mathf.PingPong() rather than Mathf.Repeat().</param>
+        /// <param name="dampened">If true, it applys linear decay to the scale.</param>
         public static IEnumerator EaseRepeat(UnityAction<float> action, Func<float, float> easingFunc, float time = 1f, float cycleCount = 3f, bool pingPong = false, bool dampened = false, bool fixedUpdate = false)
         {
             time = Mathf.Max(time, float.Epsilon);
@@ -46,6 +51,9 @@ namespace Omnis.Utils
             action?.Invoke(easingFunc(cycleCount.Repeat(1f)));
         }
 
+        /// <summary>
+        /// It performs Mathf.SmoothDamp().
+        /// </summary>
         public static IEnumerator SmoothDamp(UnityAction<float> action, float time = 1f, bool fixedUpdate = false)
         {
             time = Mathf.Max(time, float.Epsilon);
@@ -64,25 +72,27 @@ namespace Omnis.Utils
 
         #region Loops
         /// <summary>
-        /// It invokes <i>action</i> every <i>interval</i> seconds for <i>i</i> times.<br/>
-        /// If <i>interval</i> leq to 0, it invokes <i>action</i> every frame.<br/>
+        /// It invokes <i>actions</i> sequentially every <i>interval</i> seconds for <i>i</i> times.<br/>
+        /// If <i>interval</i> &lt; 0, it invokes <i>action</i> every frame.<br/>
         /// If <i>i</i> less than 1, it won't stop.
         /// </summary>
-        public static IEnumerator Loop(UnityAction action, float interval = 0f, int i = 0)
+        public static IEnumerator Loop(int i = 3, float interval = 1f, params UnityAction[] actions)
         {
-            if (action == null) yield break;
             var waitForIntervalSeconds = new WaitForSeconds(interval);
+            int it = 0;
             if (i <= 0)
                 while (true)
                 {
-                    action.Invoke();
+                    actions[it]?.Invoke();
+                    it = (it + 1) % actions.Length;
                     yield return waitForIntervalSeconds;
                 }
             else
                 while (i > 0)
                 {
-                    action.Invoke();
-                    i--;
+                    actions[it]?.Invoke();
+                    it = (it + 1) % actions.Length;
+                    if (it == 0) i--;
                     yield return waitForIntervalSeconds;
                 }
         }
