6ce7b71e5ad2f10c46a552455ffc4522bf2b0f91
Omnisch <fleurdeln@gmail.com>
2025-03-30 00:32:24 +0800

    edit MenuItem priority

--- Omnistarter/Editor/CreateMenuItem.cs
+++ Omnistarter/Editor/CreateMenuItem.cs
@@ -1,13 +1,15 @@
 // author: Omnistudio
-// version: 2025.03.15
+// version: 2025.03.30
 
 using UnityEditor;
+using UnityEngine;
 
 namespace Omnis.Editor
 {
     public static class CreateMenuItem
     {
-        [MenuItem("Assets/Create/Omnis/Singleton", false, 80)]
+        #region Assets
+        [MenuItem("Assets/Create/Omnis/Singleton", false, 241)]
         private static void CreateSingletonInstance()
         {
             string templatePath = "Assets/Omnistarter/Editor/SingletonTemplate.cs.txt";
@@ -15,11 +17,24 @@ namespace Omnis.Editor
             ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, $"{selectedObjectName}.singleton.cs");
         }
 
-        [MenuItem("Assets/Create/Omnis/Singleton", true, 80)]
+        [MenuItem("Assets/Create/Omnis/Singleton", true, 241)]
         // Validate only when the selected asset is a csharp script.
         private static bool ValidateCreateSingletonInstance()
         {
             return Selection.activeObject != null && AssetDatabase.GetAssetPath(Selection.activeObject).EndsWith(".cs");
         }
+        #endregion
+
+        #region GameObjects
+        [MenuItem("GameObject/Omnis/Input Handler", false, 241)]
+        private static void CreateInputHandler(MenuCommand menuCommand)
+        {
+            GameObject go = new("Input Handler");
+            go.AddComponent<InputHandler>();
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            Selection.activeObject = go;
+        }
+        #endregion
     }
 }
--- Omnistarter/Scripts/Audio/SoundEffectSettings.cs
+++ Omnistarter/Scripts/Audio/SoundEffectSettings.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Omnis
 {
-    [CreateAssetMenu(menuName = "Omnis/Sound Effect Settings")]
+    [CreateAssetMenu(menuName = "Omnis/Sound Effect Settings", order = 242)]
     public class SoundEffectSettings : ScriptableObject
     {
         public List<SoundEffectId> soundEffects;
