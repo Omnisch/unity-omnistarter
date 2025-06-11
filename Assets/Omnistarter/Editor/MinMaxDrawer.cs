114e50bdba85b760ebb4037386b2440d56555602
Omnisch <fleurdeln@gmail.com>
2025-06-11 17:04:52 +0800

    fix MinMax display

--- Assets/Omnistarter/Editor/MinMaxDrawer.cs
+++ Assets/Omnistarter/Editor/MinMaxDrawer.cs
@@ -11,6 +11,8 @@ namespace Omnis.Editor
     [CustomPropertyDrawer(typeof(MinMaxAttribute))]
     public class MinMaxDrawer : PropertyDrawer
     {
+        private const float gap = 4f;
+
         public override void OnGUI(Rect position, SerializedProperty minProp, GUIContent label)
         {
             var attr = (MinMaxAttribute)attribute;
@@ -22,9 +24,24 @@ namespace Omnis.Editor
                 return;
             }
 
-            EditorGUI.BeginProperty(position, label, minProp);
-            EditorTweaker.MinMaxFloat(label.text.TrimStart("Min "), minProp, maxProp);
+            Rect contentRect = EditorGUI.PrefixLabel(position, new GUIContent(label.text.TrimStart("Min ")));
+
+            float fieldWidth = (contentRect.width - gap) * 0.5f;
+            Rect minRect = new(contentRect.x, contentRect.y, fieldWidth, contentRect.height);
+            Rect maxRect = new(minRect.xMax + gap, contentRect.y, fieldWidth, contentRect.height);
+
+            float oldLabel = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 30f;
+
+            EditorGUI.BeginProperty(minRect, GUIContent.none, minProp);
+            EditorGUI.PropertyField(minRect, minProp, new GUIContent("Min"));
             EditorGUI.EndProperty();
+
+            EditorGUI.BeginProperty(maxRect, GUIContent.none, maxProp);
+            EditorGUI.PropertyField(maxRect, maxProp, new GUIContent("Max"));
+            EditorGUI.EndProperty();
+
+            EditorGUIUtility.labelWidth = oldLabel;
         }
     }
 }
--- Assets/Omnistarter/Scripts/Attributes/OmnisAttributes.cs
+++ Assets/Omnistarter/Scripts/Attributes/OmnisAttributes.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 namespace Omnis.Editor
 {
     public sealed class InspectorReadOnlyAttribute : PropertyAttribute { }
-    /// <summary>Add the attribute to the min float and hide the max float in the inspector.<br/>
-    /// E.g. [MinMax("Value")] float minValue; [HideInInspector] float maxValue;</summary>
     public sealed class MinMaxAttribute : PropertyAttribute
     {
         public readonly string pairName;
+
+        /// <summary>
+        /// Add the attribute to the min float and hide the max float in the inspector.<br/>
+        /// E.g. [MinMax("Value")] float minValue; [HideInInspector] float maxValue;
+        /// </summary>
         public MinMaxAttribute(string pairName)
         {
             this.pairName = pairName;
