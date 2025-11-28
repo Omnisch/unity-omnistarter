187de486d0cc3f1ce9ddb107281fd15097dfdc88
Omnisch <fleurdeln@gmail.com>
2025-11-28 21:52:04 +0800

    add text generation method

--- Assets/Omnistarter/Scripts/APIs/OpenAiApi.cs
+++ Assets/Omnistarter/Scripts/APIs/OpenAiApi.cs
@@ -1,5 +1,5 @@
 // author: Omnistudio
-// version: 2025.11.18
+// version: 2025.11.28
 
 using Omnis.Utils;
 using System;
@@ -12,6 +12,39 @@ namespace Omnis.API
     public static class OpenAiApi
     {
         private static readonly string BaseUrl = "https://api.openai.com/v1/responses";
+
+        /// <returns>response.output[0].content[0].text</returns>
+        public static async Task<string> TextGeneration(string apiKey, string model, string sysPrompt, string userPrompt) {
+            var request = new Request
+            {
+                model = model,
+                input = new Request.Input[] {
+                    new() {
+                        role = "developer",
+                        content = new Request.Input.Content[] {
+                            new() {
+                                type = "input_text",
+                                text = sysPrompt
+                            }
+                        }
+                    },
+                    new() {
+                        role = "user",
+                        content = new Request.Input.Content[] {
+                            new() {
+                                type = "input_text",
+                                text = userPrompt
+                            }
+                        }
+                    }
+                }
+            };
+            var requestString = JsonUtility.ToJson(request);
+            var responseRaw = await HttpHelper.PostJsonAsync(BaseUrl, $"Bearer {apiKey}", requestString);
+            var response = JsonUtility.FromJson<ChatCompletionResponse>(Encoding.UTF8.GetString(responseRaw));
+            Debug.Log($"{response.output[0].status}");
+            return response.output[0].content[0].text;
+        }
 
         /// <returns>response.output[0].content[0].text</returns>
         public static async Task<string> AnalyseImage(string apiKey, string model, string prompt, byte[] original) {
