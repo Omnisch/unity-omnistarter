d8056338d549dabb3814413b4c949b79afcc133c
Omnisch <fleurdeln@gmail.com>
2026-01-29 23:31:04 +0800

    update Recorder

--- Assets/Omnistarter/Scripts/Audio/Recorder.cs
+++ Assets/Omnistarter/Scripts/Audio/Recorder.cs
@@ -1,21 +1,21 @@
 // author: Omnistudio
-// version: 2025.12.14
+// version: 2026.01.29
 
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Omnis.Audio
 {
     public static class Recorder
     {
-        public static readonly int ClipLengthSeconds = 10;
-
-        public static AudioClip Start(int sampleRate = 16000) {
+        public static AudioClip Start(int lengthSec, int sampleRate = 16000) {
             if (Microphone.devices.Length == 0) {
                 Debug.LogError("No microphone devices found.");
                 return null;
             }
 
-            var recordingClip = Microphone.Start(null, true, ClipLengthSeconds, sampleRate);
+            var recordingClip = Microphone.Start(null, true, lengthSec, sampleRate);
             if (recordingClip == null) {
                 Debug.LogError("Failed to start microphone.");
                 return null;
@@ -25,9 +25,25 @@ namespace Omnis.Audio
             return recordingClip;
         }
 
+        public static AudioClip Record(MonoBehaviour mono, int lengthSec, int sampleRate = 16000) {
+            AudioClip clip = Start(lengthSec, sampleRate);
+
+            if (clip == null) {
+                return null;
+            }
+
+            mono.StartCoroutine(Stop(lengthSec));
+            return clip;
+        }
+
         public static void Stop() {
             Microphone.End(Microphone.devices[0]);
             Debug.Log("Recording stopped.");
+        }
+
+        private static IEnumerator Stop(int seconds) {
+            yield return new WaitForSeconds(seconds);
+            Stop();
         }
     }
 }
